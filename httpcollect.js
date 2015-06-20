2fa2813d0730f8cd704e93da008b4c833c1e07d8
Michel Tabari <tabari@kth.se>
2015-06-20 16:13:31 +0200

    Create httpcollect.js

---
+++ httpcollect.js
@@ -0,0 +1,15 @@
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function (response) {
+  var result = "";
+  response.setEncoding('utf8');
+  response.on("data", function (data) {
+    result += data;
+  });
+  response.on("end", function () {
+    console.log(result.length);
+    console.log(result);
+    });
+  });
